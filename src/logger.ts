f800d14f3f84410cc31b27b96ffd1b03af9d6646
pedro terzero <pedro@pedro>
2020-11-06 10:30:47 +0100

    'fix' logger to show call source

--- src/logger.ts
+++ src/logger.ts
@@ -9,29 +9,31 @@ export enum LogLevel {
 export const logLevel: LogLevel = LogLevel.Debug;
 
 export class Logger {
-    public debug(...args: any[]): void {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+
+    public constructor () {
+        this.initLoggers();
+    }
+
+    private initLoggers() {
         if (logLevel <= LogLevel.Debug) {
-            console.info(...args);
+            this.debug = console.info.bind(console, '[debug] ');
         }
-    }
-
-    public info(...args: any[]): void {
         if (logLevel <= LogLevel.Info) {
-            console.info(...args);
+            this.info = console.info.bind(console, '[info ] ');
         }
-    }
-
-    public warn(...args: any[]): void {
         if (logLevel <= LogLevel.Warn) {
-            console.warn(...args);
+            this.warn = console.warn.bind(console, '[warn] ');
+        }
+        if (logLevel <= LogLevel.Error) {
+            this.error = console.error.bind(console, '[error] ');
         }
     }
 
-    public error(...args: any[]): void {
-        if (logLevel <= LogLevel.Error) {
-            console.error(...args);
-        }
-    }
+    public debug(..._args: any[]): void { return; }
+    public info(..._args: any[]): void { return; }
+    public warn(..._args: any[]): void { return; }
+    public error(..._args: any[]): void { return; }
 }
 
 export const logger = new Logger();
