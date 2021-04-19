3f94de3294d313ea2a694321e1b4b1942fdb275d
pedro terzero <pedro@pedro>
2021-04-19 21:35:23 +0200

    add crlf test for itemsets

--- src/test/suite/rulesetCompletionProvider.test.ts
+++ src/test/suite/rulesetCompletionProvider.test.ts
@@ -1,10 +1,13 @@
 import * as assert from 'assert';
 import { resolve } from 'path';
-import { Position, TextDocument, workspace } from 'vscode';
+import { EndOfLine, Position, TextDocument, window, workspace } from 'vscode';
+import { rulesetResolver } from '../../extension';
 import { RulesetCompletionProvider } from '../../rulesetCompletionProvider';
+import { waitForRefresh } from './tools';
 
 const fixturePath = resolve(__dirname, '../../../src/test/suite/fixtures');
 const fixtureFilePath = resolve(fixturePath, 'completionTest.rul');
+const itemsPath = resolve(fixturePath, 'items.rul');
 
 const dummyItems = ['STR_DUMMY_ITEM', 'STR_DUMMY_ITEM2', 'STR_DUMMY_ITEM3'];
 const expectedCompletions = [
@@ -47,8 +50,34 @@ describe('rulesetCompletionProvider', () => {
             document = await workspace.openTextDocument(fixtureFilePath);
          });
 
+        after(async () => {
+             // restore this, other tests rely on it
+            const document = await workspace.openTextDocument(itemsPath);
+            await window.showTextDocument(document);
+        });
+
         it('returns the correct completions', () => {
             testCompletion(document, expectedCompletions);
+        });
+
+        it('returns the correct completions (CRLF)', async () => {
+            if (!document) {
+                throw new Error('no document');
+            }
+
+            const editor = await window.showTextDocument(document);
+
+            await editor.edit(builder => { builder.setEndOfLine(EndOfLine.CRLF); });
+            // save and wait for refresh so we check both CRLF=>LF and vice versa
+            await document.save();
+            await waitForRefresh(rulesetResolver);
+
+            testCompletion(document, expectedCompletions);
+
+            // restore
+            await editor.edit(builder => { builder.setEndOfLine(EndOfLine.LF); });
+            await document.save();
+            await waitForRefresh(rulesetResolver);
         });
     });
 });
