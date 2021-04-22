8bc479da199481664952551934cea4a011555ecf
pedro terzero <pedro@pedro>
2021-04-22 23:30:18 +0200

    v0.8.0 add context aware definition autocomplete
    Squashed commit of the following:
    
    - update yaml depencency to fix bug: https://github.com/eemeli/yaml/releases/tag/v1.10.2
    - fix bug with duplicate rules over multiple files in logic checker
    - add context aware autocomplete to docs
    - basic test for linker.yml (built in type)
    - add test for nested documentation hovers
    - add test for autocompleting at ] char
    - add tests for mapScripts: linked by terrain:
    - add crlf test for itemsets
    - subcategories in documentation
    - fix itemSets with CRLF
    - fix erroneously removed events.description
    - fix autocomplete on ]
    - add ignoreTypes
    - move (almost) all FtA stuff to linker.yml
    - recommend linker.yml for ftaGame if it's missing
    - documentation update from wiki
    - ignore errors in linker.yml, and show error
    - simpler linker config overrides
    - Fix bug for terrain: reference in mapscript command
    - allow metadata to be requested by logic class
    - allow extending of types through linker.yml
    - fix getDefinitionTypeForReference() (how did this work before?)
    - Add tests for CompletionProvider
    - Catch another edge case with findRulePath()
    - move typeLinkRegexes to typedProperties
    - Fix autocomplete with [] arrays
    - Fix autocomplete with CRLF
    - Fix >1 depth autocomplete
    - Add typelink for items.compatibleAmmo
    - Add source file, change to documentation and markdown
    - Fixed extraFiles handling to use the 'new' one from the CompletionProvider which is simpler
    - initial commit completion provider (may be some code in here that relates to other branches)

---
+++ src/rulesetCompletionProvider.ts
@@ -0,0 +1,102 @@
+import { CompletionItem, CompletionItemProvider, Position, ProviderResult, TextDocument, CompletionItemKind, workspace, WorkspaceFolder, MarkdownString } from 'vscode';
+import { KeyDetector } from './keyDetector';
+// import { i18nResolver } from './extension';
+import { logger } from './logger';
+import { rulesetTree } from './rulesetTree';
+import { typedProperties } from './typedProperties';
+import { DefinitionCompletion, DefinitionCompletions } from './workspaceFolderRuleset';
+// import { i18nTree } from './i18nTree';
+
+export class RulesetCompletionProvider implements CompletionItemProvider {
+    public provideCompletionItems(document: TextDocument, position: Position/*, _token: CancellationToken, _context: CompletionContext*/): ProviderResult<CompletionItem[]> {
+        const folder = workspace.getWorkspaceFolder(document.uri);
+        if (!folder) {
+            throw new Error(`Could not get workspace folder for ${document.uri}`);
+        }
+
+        if (!this.shouldComplete(position, document)) {
+            // logger.debug('Not completing');
+            return;
+        }
+
+        const key = KeyDetector.getAbsoluteKeyFromPositionInDocument(position, document, false);
+        const path = KeyDetector.findRulePath(position, document);
+        if (!path) {
+            return;
+        }
+
+        const target = typedProperties.getDefinitionTypeForReference(path);
+        if (!target) {
+            return;
+        }
+
+        logger.debug(`provideCompletionItems, type: ${path} key: ${key?.key}, target: ${target}`);
+
+        // const i18nKey = KeyDetector.getI18nKeyAtRangeFromDocument(range, document);
+        // let keyPrefix = i18nResolver.getDefaultLocaleKey(document.uri) + ".";
+        // logger.debug('provideCompletionItems', 'range:', range, 'i18nkey', i18nKey, 'keyPrefix', keyPrefix);
+
+        // if (KeyDetector.isRelativeKey(i18nKey)) {
+        //     keyPrefix += KeyDetector.getRelativeKeyPart(document.fileName);
+        // }
+
+        return this.buildCompletionItemList(target, key?.key, folder);
+
+        // completionItem.detail = i18nTree.lookupKey(filteredKey, workspaceFolder);
+        // return items;
+    }
+
+    /**
+     * Should autocomplete be triggered? Rules (currently) are:
+     * - should not be end of file (why?)
+     * - next char should be ",", "<space>" or "]"
+     * @param position
+     * @param document
+     * @returns
+     */
+    private shouldComplete(position: Position, document: TextDocument) {
+        let rest = document.getText().slice(document.offsetAt(position)).replace(/\r\n/g, "\n") + "\n";
+        rest = rest.slice(0, rest.indexOf("\n"));
+
+        return rest.length === 0 || [',', ' ', ']'].includes(rest.slice(0, 1));
+    }
+
+    private buildCompletionItemList(target: string, key: string | undefined, workspaceFolder: WorkspaceFolder): CompletionItem[] | undefined {
+        const filteredKeys = rulesetTree.getKeysContaining(key, target, workspaceFolder);
+        if (!filteredKeys || Object.keys(filteredKeys).length === 0) {
+            return;
+        }
+
+        logger.debug(`buildCompletionItemList filteredKeys: ${Object.keys(filteredKeys).length}`);
+        return this.transformFilterResultIntoCompletionItemList(filteredKeys);//, keyPrefix, i18nKey, workspaceFolder);
+    }
+
+    private transformFilterResultIntoCompletionItemList(filteredKeys: DefinitionCompletions/*, prefixToRemove: string, i18nKeyToComplete: string, workspaceFolder: WorkspaceFolder*/): CompletionItem[] {
+        const ret = [];
+        for (const key in filteredKeys) {
+            ret.push(this.buildCompletionItem(key, filteredKeys[key]));
+        }
+        return ret;
+
+        // return filteredKeys.map(filteredKey => {
+        //     return this.buildCompletionItem(filteredKey);//, prefixToRemove, i18nKeyToComplete, workspaceFolder);
+        // });
+    }
+
+    private buildCompletionItem(filteredKey: string, data: DefinitionCompletion/*, prefixToRemove: string, i18nKeyToComplete: string, workspaceFolder: WorkspaceFolder*/): CompletionItem {
+        // remove the prefix (locale key and possibly key-part relative to current file location)
+        // let relevantKey = filteredKey.substring(prefixToRemove.length);
+        // use the relevant key part as label for completion item
+        const completionItem = new CompletionItem(filteredKey, CompletionItemKind.Reference);
+        // current word gets replaced, so we need to provide the current full keypart that is being typed
+        // completionItem.insertText = filteredKey;
+        // completionItem.insertText = relevantKey.substring(i18nKeyToComplete.lastIndexOf(".") + 1);
+        // provide the translation as additional info
+        // completionItem.detail = i18nTree.lookupKey(filteredKey, workspaceFolder);
+        if ('detail' in data) {
+            completionItem.documentation = new MarkdownString(data.detail);
+        }
+
+        return completionItem;
+    }
+}
