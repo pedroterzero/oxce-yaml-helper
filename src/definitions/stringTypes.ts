12e758d5da63165395e4e5759c8c3d2e1f807647
pedro terzero <pedro@pedro>
2021-05-12 20:14:51 +0200

    - no longer check ufopaedia.weapon (it's a translatable string)

--- src/definitions/stringTypes.ts
+++ src/definitions/stringTypes.ts
@@ -34,6 +34,7 @@ export const stringTypes = ([] as string[]).concat([
     'ufopaedia.text',
     'ufopaedia.pages[].text',
     'ufopaedia.pages[].title',
+    'ufopaedia.weapon',
     // not sure
     // 'items.categories',
     // 'manufacture.category',
