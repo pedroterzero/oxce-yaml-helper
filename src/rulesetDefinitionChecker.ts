9cecdfcaf8ffc9a2e4f9b4085f5b8c6a14fe678a
pedro terzero <pedro@pedro>
2021-06-04 14:01:04 +0200

    v0.8.2: many fixes
    Squashed commit of the following:
    
        bump version, bump package
        #26 fix reloading of Language/*.yml files
        fix test docker
        Update changelog
        - Fix refnodes in logic checkers (may need to add in other positions)
        - fix ufopaedia test
        bump version
        - no longer check ufopaedia.weapon (it's a translatable string)
        - get comments in extra files
        - enable subX/subY for BIGOBS and FLOOROBS
        - add hitMissSound builtins
        - added missing stringType
        - prepare for more extraSprites subY/subX
        - combine alienMissions data
        - ufopaedia image not required for type 3, apparently
        - manufacture.requiredItems add crafts
        - fix autocomplete for line that is also start of an array entry
        - improve duplicate error message (use related information, #35)
        - fix relatedlogic fields overwriting existing typeLinks
        - added missing type link for alienMissions.waves[].ufo
        - fixed missing lt & le in y-script highlighting
        - fix message for missing group in addBlock
        - "check" (store) logic data for vanilla resources
        - fix multi spritesheets for INTICON.PCK
        - ignore aliases when finding definitions
        - add missing builtins

--- src/rulesetDefinitionChecker.ts
+++ src/rulesetDefinitionChecker.ts
@@ -1,4 +1,4 @@
-import { Diagnostic, DiagnosticSeverity, Range, Uri, workspace } from "vscode";
+import { Diagnostic, DiagnosticRelatedInformation, DiagnosticSeverity, Location, Range, Uri, workspace } from "vscode";
 import { DefinitionLookup, Match } from "./rulesetTree";
 import { ReferenceFile, TypeLookup, WorkspaceFolderRuleset } from "./workspaceFolderRuleset";
 import { soundTypeLinks, spriteTypeLinks, typeLinks, typeLinksPossibleKeys } from "./definitions/typeLinks";
@@ -62,19 +62,30 @@ export class RulesetDefinitionChecker {
         this.logicHandler = new LogicHandler;
     }
 
-    public checkFile(file: ReferenceFile, ruleset: WorkspaceFolderRuleset, workspacePath: string, hierarchy: WorkspaceFolderRulesetHierarchy): Diagnostic[] {
+    public checkFile(file: ReferenceFile, ruleset: WorkspaceFolderRuleset, _workspacePath: string, hierarchy: WorkspaceFolderRulesetHierarchy): Diagnostic[] {
         const diagnostics : Diagnostic[] = [];
 
         this.checkReferences(file, ruleset.definitionsLookup, diagnostics);
-        this.addDuplicateDefinitions(file, diagnostics, workspacePath);
+        this.addDuplicateDefinitions(file, diagnostics);
 
+        this.checkLogicData(ruleset, file, diagnostics, hierarchy);
+
+        return diagnostics;
+    }
+
+    /**
+     * Separate call to check logic data to allow data to be collected from vanilla assets
+     * @param ruleset
+     * @param file
+     * @param diagnostics
+     * @param hierarchy
+     */
+    public checkLogicData(ruleset: WorkspaceFolderRuleset, file: ReferenceFile, diagnostics: Diagnostic[], hierarchy: WorkspaceFolderRulesetHierarchy) {
         let logicData;
         if ((logicData = ruleset.getLogicData(file.file))) {
             // console.log(`checking! ${file.file.path}`);
             this.logicHandler.check(file.file, diagnostics, logicData, hierarchy);
         }
-
-        return diagnostics;
     }
 
     public checkRelationLogic(diagnosticsPerFile: FilesWithDiagnostics): FilesWithDiagnostics {
@@ -83,26 +94,29 @@ export class RulesetDefinitionChecker {
         return mergeAndConcat(diagnosticsPerFile, additionalFilesWithDiagnostics) as FilesWithDiagnostics;
     }
 
-    private addDuplicateDefinitions(file: ReferenceFile, diagnostics: Diagnostic[], workspacePath: string) {
+    private addDuplicateDefinitions(file: ReferenceFile, diagnostics: Diagnostic[]) {
         if (!(file.file.path in this.duplicatesPerFile)) {
             return;
         }
 
         const duplicates = this.duplicatesPerFile[file.file.path];
-        // const messages = [];
         for (const duplicate of duplicates) {
-            const parts = [];
+            const relatedInformation = [];
             for (const dupdef of duplicate.duplicates) {
-                parts.push(`${dupdef.file.path.slice(workspacePath.length + 1)} line ${dupdef.rangePosition[0][0]}`);
+                relatedInformation.push(
+                    new DiagnosticRelatedInformation(new Location(dupdef.file, new Range(...dupdef.rangePosition[0], ...dupdef.rangePosition[1])), 'also defined here')
+                );
             }
 
-            const message = `${duplicate.definition.type} ${duplicate.key} is duplicate, also exists in (add # ignoreDuplicate after this to ignore this entry):\n\t${parts.join("\n\t")}`;
+            const message = `${duplicate.definition.type} ${duplicate.key} is duplicate (add # ignoreDuplicate after this to ignore this entry)`;
 
             const range = new Range(...duplicate.definition.rangePosition[0], ...duplicate.definition.rangePosition[1]);
-
-            diagnostics.push(new Diagnostic(range, message, DiagnosticSeverity.Warning));
-            // messages.push(message);
-
+            diagnostics.push({
+                range,
+                message,
+                severity: DiagnosticSeverity.Warning,
+                relatedInformation
+            });
         }
 
         // appendFile(workspacePath + '/messages.txt', doc.fileName.slice(workspacePath.length + 1) + "\n==========\n" + messages.join("\n") + "\n\n", () => { return; });
