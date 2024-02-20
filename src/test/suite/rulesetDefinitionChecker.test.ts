c0cb34c217200f85f9b4b20143d897f8fcebfd52
pedro terzero <pedro@pedro>
2024-02-20 02:29:17 +0100

    fix tests

--- src/test/suite/rulesetDefinitionChecker.test.ts
+++ src/test/suite/rulesetDefinitionChecker.test.ts
@@ -53,7 +53,7 @@ const getNumberOfDiagnostics = () => {
 };
 
 // this is useful because it gives a general idea if diagnostics are correctly generated without explicitly checking each one
-const expectedNumberOfDiagnostics = 348;
+const expectedNumberOfDiagnostics = 563;
 
 const originalSettingFindDuplicateDefinitions = workspace
     .getConfiguration('oxcYamlHelper')
